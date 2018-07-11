53f35efd1e7396f58cc4dfb585c3f33f33f19560
Carlos Tiago <ontiago@gmail.com>
2018-07-11 13:49:10 -0300

    App básico finalizado

--- App.js
+++ App.js
@@ -6,24 +6,26 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
+const geraNumeroAleatorio = () => {
+  let numeroAleatorio = Math.random();
+  numeroAleatorio = Math.floor(numeroAleatorio * 10);
+  alert(numeroAleatorio);
+};
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>Gerador de números randômicos</Text>
+        <TouchableOpacity
+          style={styles.Btn}
+          onPress={geraNumeroAleatorio}
+        >
+          <Text style={styles.BtnText}>Gerar número randômico</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -36,14 +38,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  BtnText: {
+    color: '#fff'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  Btn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#841584',
+    padding: 10,
   },
 });
